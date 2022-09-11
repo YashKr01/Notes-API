6fdf660203e882c667090029e8c9d898d0ae5f90
YashKr01 <yashkumar201301@gmail.com>
2022-09-11 16:09:55 +0530

    local API setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World!!")
+const express = require("express");
+const app = express();
+
+const quotes = require("./quotes.json")
+
+app.get("/quote", (req, res) => {
+    res.status(200).json(quotes);
+});
+
+app.get("/random", (req, res) => {
+    let index = Math.floor(Math.random() * quotes.length);
+    let quote = quotes[index];
+    res.status(404).json(quote);
+});
+
+app.listen(6756, () => {
+    console.log("Server is started");
+});
