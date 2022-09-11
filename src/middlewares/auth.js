1f6116105ece1f22b2c36a523ce62adf415dcc19
YashKr01 <yashkumar201301@gmail.com>
2022-09-11 23:08:28 +0530

    Notes CRUD Operations

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,10 +1,9 @@
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = process.env.SECRET_KEY;
+const SECRET_KEY = "Notes";
 
 const auth = (req, res, next) => {
 
     try {
-
         let token = req.headers.authorization;
         if (token) {
             token = token.split(" ")[1];
